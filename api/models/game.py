852f82ec103424d626715bd41d274e20b811661a
nat <nat.lownes@gmail.com>
2011-04-10 10:45:43 -0400

    moving models into separate files

---
+++ api/models/game.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Game(models.Model):
+  attendance = models.IntegerField()
+
--- api/models/player.py
+++ api/models/player.py
@@ -1,18 +1,10 @@
 from django.db import models
 
-class Team(models.Model):
-  team_abbreviation = models.CharField(max_length=50) 
-  league            = models.CharField(max_length=50)
-  city              = models.CharField(max_length=200)
-  nickname          = models.CharField(max_length=200)
-  year_first_active   = models.CharField(max_length=4)
-  year_last_active    = models.CharField(max_length=4)
-
 class Player(models.Model):
   first_name = models.CharField()
   last_name  = models.CharField()
   retrosheet_id = models.CharField()
-  debut_date = models.DateField()
+  debut_date = models.DateField(null=True)
   #debut_year = models.IntegerField()
   #debut_month = models.IntegerField()
   #debut_day = models.IntegerField()
